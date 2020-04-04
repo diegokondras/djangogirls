dd771b370f4b688f6f31ea8078878b2a5d440beb
Diego Eduardo Kondras <diegokondras@gmail.com>
2020-04-04 18:14:30 -0300

    Extensions

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,12 @@
 from .models import Post, Comment
+from django import forms
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
+
 class CommentForm(forms.ModelForm):
 
     class Meta:
